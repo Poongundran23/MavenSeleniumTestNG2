9645f588f74b6b6b0d0ba385cf49452703280207
Poongundran23 <poongundran27@gmail.com>
2023-09-23 11:49:04 +0530

    Update AdactinSearchHotelPage.java
    adHP

--- src/main/java/pageElements/AdactinSearchHotelPage.java
+++ src/main/java/pageElements/AdactinSearchHotelPage.java
@@ -1,6 +1,11 @@
 package pageElements;
 
+import org.openqa.selenium.support.PageFactory;
 import qaBase.BasePage;
 
 public class AdactinSearchHotelPage extends BasePage {
+
+    public AdactinSearchHotelPage() {
+        PageFactory.initElements(driver,this);
+    }
 }
